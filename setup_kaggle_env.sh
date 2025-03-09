33eccbcf38f594761b66a23244b4bd60fa0ba156
0xpix <ns.chlfat@gmail.com>
2025-03-09 07:18:24 +0100

    Refactor setup script to install Python via apt instead of manual installation

--- setup_kaggle_env.sh
+++ setup_kaggle_env.sh
@@ -9,22 +9,29 @@ sudo apt install -y software-properties-common build-essential zlib1g-dev libffi
     libsqlite3-dev libbz2-dev liblzma-dev libreadline-dev libncursesw5-dev libgdbm-dev tk-dev \
     libgdbm-compat-dev libdb-dev gcc make wget curl
 
-echo "🐍 Installing Python 3.12.4..."
-PYTHON_VERSION="3.12.4"
-cd /usr/src
-sudo rm -rf Python-${PYTHON_VERSION}*
-sudo wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
-sudo tar xvf Python-${PYTHON_VERSION}.tgz
-cd Python-${PYTHON_VERSION}
-sudo ./configure --enable-optimizations
-sudo make -j$(nproc)
-sudo make altinstall
+# echo "🐍 Installing Python 3.12.4..."
+# PYTHON_VERSION="3.12.4"
+# cd /usr/src
+# sudo rm -rf Python-${PYTHON_VERSION}*
+# sudo wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
+# sudo tar xvf Python-${PYTHON_VERSION}.tgz
+# cd Python-${PYTHON_VERSION}
+# sudo ./configure --enable-optimizations
+# sudo make -j$(nproc)
+# sudo make altinstall
 
-echo "✅ Python 3.12.4 installed!"
-python3.12 --version
+# echo "✅ Python 3.12.4 installed!"
+# python3.12 --version
+
+echo "🐍 Install Python"
+sudo apt update
+sudo apt install python3 python3-pip python3-venv
 
 echo "🔗 Setting 'python' to point to 'python3'..."
-sudo ln -sf /usr/local/bin/python3.12 /usr/bin/python
+# sudo ln -sf /usr/local/bin/python3.12 /usr/bin/python
+# python --version
+sudo rm /usr/bin/python
+sudo ln -s /usr/bin/python3 /usr/bin/python
 python --version
 
 echo "📦 Upgrading pip..."
